e62a0535289fea8182dd4eae9977ddfdcab1a853
Nashir-ngb <nashirngb@gmail.com>
2025-08-03 11:51:08 +0800

    Footer update

--- unimarket-frontend/src/App.js
+++ unimarket-frontend/src/App.js
@@ -27,7 +27,9 @@ export default function App() {
   return (
     <AuthProvider>
       <Router>
+        <div className="flex flex-col min-h-screen">
         <Navbar />
+        <main className="flex-1">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<Products />} />
@@ -47,8 +49,10 @@ export default function App() {
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/about" element={<About />} />
         </Routes>
+        </main>
         <ToastContainer position="top-center" />
         <Footer />
+        </div>
       </Router>
     </AuthProvider>
   );
